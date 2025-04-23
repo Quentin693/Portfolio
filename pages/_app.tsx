51854882b6cc53e68e4bd6bf19b1955b2f0bbf53
Cialone Quentin <quentinho@MacBook-Air-de-Cialone.local>
2025-04-24 01:24:16 +0200

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
 import type { AppProps } from 'next/app'
 import '../styles/globals.css'  // Importez votre CSS global ici
+import Head from 'next/head'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Portfolio 3D Interactif</title>
+        <meta name="description" content="Portfolio créatif et interactif en 3D avec Next.js, Three.js et Tailwind CSS" />
+      </Head>
+      <main className={inter.className}>
+        <Component {...pageProps} />
+      </main>
+    </>
+  )
 }
 
 export default MyApp
